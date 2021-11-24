8b757c9d76df73733bf220400aa5314fd23e6615
Robert Strojny <robert.strojny95@gmail.com>
2021-11-24 13:57:13 +0100

    Added Bmi excercise

---
+++ week-01/day-3/src/Bmi.java
@@ -0,0 +1,7 @@
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+        System.out.println("BMI index is : " + (massInKg / (heightInM*heightInM)));
+    }
+}
