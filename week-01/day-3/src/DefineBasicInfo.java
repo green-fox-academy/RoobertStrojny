4e2126aa63b827d137f8e7707f4d3985936c3a0c
Robert Strojny <robert.strojny95@gmail.com>
2021-11-24 14:01:45 +0100

    Added DefineBasicInfo excercise

---
+++ week-01/day-3/src/DefineBasicInfo.java
@@ -0,0 +1,12 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Robert Strojny";
+        int age = 26;
+        double height = 1.92;
+        boolean married = false;
+        System.out.println("My name is " + name);
+        System.out.println("My age is " +age);
+        System.out.println("My height is " +height);
+        System.out.println("My marriage status is " +married);
+    }
+}
