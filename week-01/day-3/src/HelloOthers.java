86a4f1554c5c52d4f6c988b37ddc4374d1f9c84a
Robert Strojny <robert.strojny95@gmail.com>
2021-11-24 13:22:51 +0100

    added excercises HelloMe, HelloOthers, HumptyDumpty, IntroduceYourself

---
+++ week-01/day-3/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Stefan!");
+        System.out.println("Hello, Michal!");
+        System.out.println("Hello, Jakub!");
+    }
+}
---
+++ week-01/day-3/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Robert Strojny");
+        System.out.println(26);
+        System.out.println(1.92);
+    }
+}
