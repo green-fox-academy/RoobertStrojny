bec76fd49d44ad1204a5045872d954ff6e46088c
Robert Strojny <robert.strojny95@gmail.com>
2021-11-24 13:42:08 +0100

    Added CodingHours excersise

---
+++ week-01/day-3/src/CodingHours.java
@@ -0,0 +1,8 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        System.out.println("An average Green Fox attendee codes 6 hours daily");
+        System.out.println("The semester is 17 weeks long");
+        System.out.println("Average attendee will code " + (17*5*6) + " hours without weekends.");
+        System.out.println("Percentage of coding hours in semester is: " + ((52)/(7*24.))*100 + " % weekly.");
+    }
+}
