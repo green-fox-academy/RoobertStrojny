622d088a2040a710c095a0fb909159e4ea61cdbd
Robert Strojny <robert.strojny95@gmail.com>
2021-11-24 13:30:03 +0100

    Added TwoNumbers excersise

---
+++ week-01/day-3/src/TwoNumbers.java
@@ -0,0 +1,10 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        System.out.println("22 + 13 = : " + (22+13));
+        System.out.println("22 - 13 = : " + (22-13));
+        System.out.println("22 * 13 = : " + (22*13));
+        System.out.println("22 / 13 = : " + (22./13));
+        System.out.println("Integer of 22 / 13 = : " + (22/13));
+        System.out.println("Remainder of 22 / 13 = : " + (22%13));
+    }
+}
