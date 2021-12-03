2a70cd0f7f8317301150ac42318baeef6cab5286
Robert Strojny <robert.strojny95@gmail.com>
2021-12-03 12:19:05 +0100

    Add update to LinePlayQuarters in DrawingProject

--- week-02/day-5/src/LinePlayQuarters.java
+++ week-02/day-5/src/LinePlayQuarters.java
@@ -2,58 +2,46 @@ import javax.swing.*;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class LinePlayQuarters {
-    public static void drawGreenLine(int x0, int y0, int x1, int y1, Graphics graphics){
+    public static void drawGreenLine(int fix, int x0, int y1, Graphics graphics){
         graphics.setColor(Color.green);
-        graphics.drawLine(x0, y0, x1, y1);
+        graphics.drawLine(x0 , fix, fix, y1);
     }
 
-    public static void drawPurpleLine(int x0, int y0, int x1, int y1, Graphics graphics){
+    public static void drawPurpleLine(int fix, int y0, int x1, Graphics graphics){
         graphics.setColor(Color.MAGENTA);
-        graphics.drawLine(x0, y0, x1, y1);
+        graphics.drawLine(fix, y0, x1, fix);
     }
 
-    public static void drawQuarter(int x0, int y0, int size, Graphics graphics){
-        graphics.drawRect(x0, y0, size, size);
-    }
 
     public static void drawImage(Graphics graphics) {
-
-//        for (int x = 20; x < WIDTH; x+=20) {
-//            drawPurpleLine(x, graphics);
-//        }
-//
-//        for (int x = 20; x < HEIGHT; x+=20) {
-//            drawGreenLine(x, graphics);
-//        }
-        int size = WIDTH/2;
-//        ArrayList<Integer> xPoints = new ArrayList<>();
-//        ArrayList<Integer> yPoints = new ArrayList<>();
-//        for (int i = 0; i <= WIDTH; i += size) {
-//            xPoints.add(i);
-//        }
-//        for (int i = 0; i <= HEIGHT; i+=size) {
-//            yPoints.add(i);
-//        }
-
-
-        for (int i = 0; i <= HEIGHT; i+=size) {
-            for (int j = 0; j <= WIDTH; j+=size) {
-                drawQuarter(j, i, size, graphics);
-                for (int x = size/16; x < size; x+=(size/16)) {
-                    drawPurpleLine(x, i, j, x, graphics);
-                }
-                for (int x = size/16; x < size; x+=(size/16)) {
-                    drawGreenLine(j, x, x, i, graphics);
-                }
-            }
+        List<Integer> edgePoints = new ArrayList<>();
+        int divider = 4;
+        int distanceBetweenLines = 4;
+        int size = WIDTH/divider;
+        for (int i = 0; i <= WIDTH; i+=size) {
+            edgePoints.add(i);
         }
 
-
+        for (int y = 0; y < divider; y++) {
+            for (int x = 0; x < divider; x++) {
+                for (int green = edgePoints.get(x) + distanceBetweenLines; green < (edgePoints.get(x) + size); green += distanceBetweenLines) {
+                    drawGreenLine(green, edgePoints.get(x), edgePoints.get(x) + size, graphics);
+                }
+                for (int purple = edgePoints.get(x); purple <= edgePoints.get(x) + size; purple += distanceBetweenLines) {
+                    drawPurpleLine(purple, edgePoints.get(x), edgePoints.get(x) + size, graphics);
+                }
+            }
+                
+        }
+            
     }
+
+
 
     // Don't touch the code below
     static int WIDTH = 320;
