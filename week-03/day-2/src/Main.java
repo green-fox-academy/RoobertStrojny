b930ca7705dbab32c6ef14afed13c0d6af78e801
Robert Strojny <robert.strojny95@gmail.com>
2021-12-07 13:42:42 +0100

    Add BlogPost and Animals exercise

--- week-03/day-2/src/Main.java
+++ week-03/day-2/src/Main.java
@@ -1,15 +1,37 @@
+import animals.Animals;
+import blogpost.BlogPost;
 import postit.PostIt;
 
 public class Main {
     public static void main(String[] args) {
         createPostItObjects();
+        createBlogPostObjects();
+        animalsObjects();
 
 
     }
 
-    public static void createPostItObjects(){
+    public static void createPostItObjects() {
         PostIt postIt1 = new PostIt("orange", "Idea 1", "blue");
         PostIt postIt2 = new PostIt("pink", "Awesome", "yellow");
         PostIt postIt3 = new PostIt("yellow", "Superb!", "green");
+    }
+
+    public static void createBlogPostObjects() {
+        BlogPost blogPost1 = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet."
+                , "2000.05.04.");
+        BlogPost blogPost2 = new BlogPost("Tim Urban", "Wait but why"
+                , "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+        BlogPost blogPost3 = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump"
+                , "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention." +
+                " When I asked to take his picture outside one of IBM’s New York City offices," +
+                " he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
+    }
+
+    public static void animalsObjects(){
+        Animals dog = new Animals("dog");
+        dog.eat();
+        dog.drink();
+        dog.play();
     }
 }
---
+++ week-03/day-2/src/animals/Animals.java
@@ -0,0 +1,27 @@
+package animals;
+
+public class Animals {
+    String name;
+    int hunger = 50;
+    int thirst = 50;
+
+    public Animals(String name){
+        this.name = name;
+    }
+
+    public void eat(){
+        hunger--;
+        System.out.println("Hunger decreased by one. Current hunger: " + hunger);
+    }
+
+    public void drink(){
+        thirst--;
+        System.out.println("Thirst decreased by one. Current thirst: " + thirst);
+    }
+
+    public void play(){
+        hunger++;
+        thirst++;
+        System.out.println("Hunger and thirst increased by one. Current hunger : " + hunger + ", thirst : " +thirst);
+    }
+}
