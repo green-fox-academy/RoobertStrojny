e145c670fa92af9048d0c21ec85f71dce22b6b14
Robert Strojny <robert.strojny95@gmail.com>
2021-12-07 15:36:18 +0100

    Add Pokemon exercise

---
+++ week-03/day-2/src/pokemon/Main.java
@@ -0,0 +1,47 @@
+package pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Pokemon> pokemonListOfAsh = initializePokemons();
+
+        // Every pokemon has a name and a type.
+        // Certain types are effective against others, e.g. water is effective against fire.
+
+        // Ash has a few pokemon.
+        // A wild pokemon appeared!
+
+
+        Pokemon wildPokemon = new Pokemon("Oddish", "grass", "water");
+
+        // Which pokemon should Ash use?
+        // Write the necessary code to get the name of the right pokemon and print it:
+
+        String rightPokemon = null;
+
+        for (Pokemon listOfAsh : pokemonListOfAsh) {
+            if (listOfAsh.isEffectiveAgainst(wildPokemon)) {
+                rightPokemon = listOfAsh.name;
+            }
+        }
+
+
+
+
+        System.out.print("I choose you, " + rightPokemon);
+    }
+
+    private static List<Pokemon> initializePokemons() {
+        List<Pokemon> pokemon = new ArrayList<>();
+
+        pokemon.add(new Pokemon("Bulbasaur", "grass", "water"));
+        pokemon.add(new Pokemon("Pikachu", "electric", "water"));
+        pokemon.add(new Pokemon("Charizard", "fire", "grass"));
+        pokemon.add(new Pokemon("Pidgeot", "flying", "fighting"));
+        pokemon.add(new Pokemon("Kingler", "water", "fire"));
+
+        return pokemon;
+    }
+}
