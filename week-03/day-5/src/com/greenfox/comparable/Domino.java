9c96b3cb8930c0c2115fb8c6e984081c8dee9b68
Robert Strojny <robert.strojny95@gmail.com>
2021-12-10 14:41:59 +0100

    Add Comparable exercise for Domino

---
+++ week-03/day-5/src/com/greenfox/comparable/Domino.java
@@ -0,0 +1,32 @@
+package com.greenfox.comparable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Domino implements Comparable<Domino> {
+    private final int left;
+    private final int right;
+
+    public Domino(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeftSide() {
+        return left;
+    }
+
+    public int getRightSide() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + left + ", " + right + "]";
+    }
+
+    @Override
+    public int compareTo(Domino domino) {
+            return this.getLeftSide() - domino.getLeftSide();
+    }
+}
