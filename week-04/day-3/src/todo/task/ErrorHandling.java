f443b5e68900ac42f83167aa8824c7f5ef1fe741
Robert Strojny <robert.strojny95@gmail.com>
2021-12-16 09:49:19 +0100

    add makeListAndCheckFileError method

--- week-04/day-3/src/todo/task/ErrorHandling.java
+++ week-04/day-3/src/todo/task/ErrorHandling.java
@@ -1,10 +1,26 @@
 package todo.task;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ErrorHandling {
-    public void path() {
+
+    public Path path() {
         Path path = Paths.get("todo/task/tasks.txt");
+        return path;
+    }
+
+    public List<String> makeListAndCheckFileError() {
+        try {
+            List<String> taskList = Files.readAllLines(path());
+            return taskList;
+        } catch (IOException e) {
+            System.out.println("Can't find file path!");
+            return new ArrayList<>();
+        }
     }
 }
