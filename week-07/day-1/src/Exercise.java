a9f08b402dea046b33442ac00a3a1392631346f2
Robert Strojny <robert.strojny95@gmail.com>
2022-01-17 12:30:56 +0100

    add even numbers exercise

--- week-07/day-1/src/Exercise.java
+++ week-07/day-1/src/Exercise.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise {
+
+    public static void main(String[] args) {
+        evenNumbers();
+    }
+
+    public static void evenNumbers() {
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .toList();
+
+        System.out.println(evenNumbers);
+
+        List<Integer> expectedOutput = Arrays.asList(-2, -4, -8, 12, 6, 10, 14);
+    }
 }
