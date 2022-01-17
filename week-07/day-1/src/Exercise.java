6c48ac468792ae602a8cbf89617d3bcce72ecf97
Robert Strojny <robert.strojny95@gmail.com>
2022-01-17 13:15:30 +0100

    add square number exercise

--- week-07/day-1/src/Exercise.java
+++ week-07/day-1/src/Exercise.java
@@ -1,20 +1,40 @@
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise {
 
     public static void main(String[] args) {
-        evenNumbers();
+//        evenNumbers();
+        squareNumbers();
     }
 
-    public static void evenNumbers() {
+    private static List<Integer> squareNumbers() {
+
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        List<Integer> squareNumbers = numbers.stream()
+                .filter(n -> n > 0)
+                .map(n -> n = n * n)
+                .collect(Collectors.toList());
+        System.out.println(squareNumbers);
+
+        List<Integer> expectedOutput = Arrays.asList(1, 9, 144, 361, 36, 81, 100, 196);
+        System.out.println(expectedOutput);
+
+        return squareNumbers;
+    }
+
+    public static List<Integer> evenNumbers() {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
         List<Integer> evenNumbers = numbers.stream()
                 .filter(n -> n % 2 == 0)
                 .toList();
 
         System.out.println(evenNumbers);
-
         List<Integer> expectedOutput = Arrays.asList(-2, -4, -8, 12, 6, 10, 14);
+        System.out.println(expectedOutput);
+
+        return evenNumbers;
     }
 }
