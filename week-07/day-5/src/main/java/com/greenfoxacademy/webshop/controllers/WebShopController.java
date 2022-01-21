bdf94be923472c9edc51837bc0cf8aaaaa2fe317
Robert Strojny <robert.strojny95@gmail.com>
2022-01-21 16:31:59 +0100

    add only available, cheapest first and contains nike get mapping

--- week-07/day-5/src/main/java/com/greenfoxacademy/webshop/controllers/WebShopController.java
+++ week-07/day-5/src/main/java/com/greenfoxacademy/webshop/controllers/WebShopController.java
@@ -2,29 +2,57 @@ package com.greenfoxacademy.webshop.controllers;
 
 import com.greenfoxacademy.webshop.models.ShopItem;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("")
 public class WebShopController {
 
-    List<ShopItem> items = new ArrayList<>();
+    List<ShopItem> items;
 
     @GetMapping("/webshop")
-    public String index(){
+    public String index(Model model) {
+        List<ShopItem> items = addItems();
+        model.addAttribute("items", items);
         return "index";
     }
 
-    private void addItems() {
+    @GetMapping("only-available")
+    public String available(Model model) {
+        List<ShopItem> items = addItems().stream().filter(n -> n.getQuantityOfStock() > 0).collect(Collectors.toList());
+        model.addAttribute("items", items);
+        return "index";
+    }
+
+    @GetMapping("contains-nike")
+    public String containsNike(Model model) {
+        String nike = "Nike";
+        List<ShopItem> items = addItems().stream().filter(n -> n.getDescription().contains(nike)).collect(Collectors.toList());
+        model.addAttribute("items", items);
+        return "index";
+    }
+
+    @GetMapping("cheapest-first")
+    public String cheapestFirst(Model model) {
+        List<ShopItem> items = addItems().stream().sorted(Comparator.comparingDouble(ShopItem::getPrice)).collect(Collectors.toList());
+        model.addAttribute("items", items);
+        return "index";
+    }
+
+    private List<ShopItem> addItems() {
+        List<ShopItem> items = new ArrayList<>();
         items.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000.0, 5));
         items.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000.0, 2));
         items.add(new ShopItem("Coca cola", "0.5l standard coke", 25.0, 0));
         items.add(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119.0, 100));
         items.add(new ShopItem("T-shirt", "Blue with corgi on a bike", 300.0, 1));
+        return items;
     }
 }
