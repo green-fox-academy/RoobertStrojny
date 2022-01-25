a09b0797890c37b39a3f37f7c2da2dc6762043cc
Robert Strojny <robert.strojny95@gmail.com>
2022-01-25 16:55:02 +0100

    add greenfox class app controller, service, template

---
+++ week-08/day-2/src/main/java/com/greenfox/di/gfclassapp/controllers/GreenfoxAppController.java
@@ -0,0 +1,30 @@
+package com.greenfox.di.gfclassapp.controllers;
+
+import com.greenfox.di.gfclassapp.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class GreenfoxAppController {
+    private StudentService studentService;
+
+    @Autowired
+    public GreenfoxAppController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("gfa")
+    public String index(){
+        return "gfa";
+    }
+
+    @GetMapping("gfa/list")
+    public String list(Model model){
+        model.addAttribute("students", studentService.findAll());
+        return "list";
+    }
+}
