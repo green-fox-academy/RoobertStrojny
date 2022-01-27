fcddc2245447e158a058445c79ba3e7a48912856
Robert Strojny <robert.strojny95@gmail.com>
2022-01-27 11:13:13 +0100

    add mappings for login

--- week-08/day-4/src/main/java/com/greenfox/programmerfoxclub/controllers/MainController.java
+++ week-08/day-4/src/main/java/com/greenfox/programmerfoxclub/controllers/MainController.java
@@ -1,15 +1,29 @@
 package com.greenfox.programmerfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
-    @GetMapping
-    public String index() {
+    @GetMapping("/")
+    public String index(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@RequestParam(name = "name") String name, RedirectAttributes redirectAttributes, Model model) {
+        model.addAttribute("name", name);
+        redirectAttributes.addAttribute("name", name);
+        return "redirect:/";
     }
 }
---
+++ week-08/day-4/src/main/java/com/greenfox/programmerfoxclub/models/Fox.java
@@ -0,0 +1,13 @@
+package com.greenfox.programmerfoxclub.models;
+
+public class Fox {
+    private final String name;
+
+    public Fox(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
