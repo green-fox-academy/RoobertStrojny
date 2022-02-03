5db9de39004f187c12a343c88e8195506eb37aa7
Robert Strojny <robert.strojny95@gmail.com>
2022-02-03 14:43:04 +0100

    add registration for user

--- week-09/day-4/src/main/java/com/greenfox/reddit/controllers/UserController.java
+++ week-09/day-4/src/main/java/com/greenfox/reddit/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.greenfox.reddit.controllers;
 
+import com.greenfox.reddit.models.User;
 import com.greenfox.reddit.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -14,5 +19,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/register")
+    public String registerView(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
 
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginView(Model model) {
+        return "login";
+    }
 }
